1b1cb1df5d559fc1b710900827dbddc46d0a9e7a
Denys Bondarchuk <profjuvi@gmail.com>
2024-08-13 01:53:08 +0200

    Add the intro

---
+++ c/01_intro/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // Preprocessor directive to include the standard input/output library
+
+int main() {
+    // Print a message to the screen
+    printf("Hello, World!\n");
+    return 0; // Return from the main function
+}
